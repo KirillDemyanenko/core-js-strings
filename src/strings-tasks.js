ac8c21224c114796e965d8f8c6c9bf9dfff5973b
KirillDemyanenko <demyanenkokg@gmail.com>
2024-12-03 17:13:29 +0300

    feat: orderAlphabetically

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -268,8 +268,11 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str
+    .split('')
+    .sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0))
+    .join('');
 }
 
 /**
